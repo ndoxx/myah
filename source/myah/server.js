8c7577dafaa63afec6c2fc4a3c4babd8f81c046c
ndoxx <bg.physique@gmail.com>
2020-08-26 16:16:58 +0200

    More secure socket handshake using io middleware

--- source/myah/server.js
+++ source/myah/server.js
@@ -63,7 +63,7 @@ app.post('/login', (req, res) => {
     Users.forEach(function(user) {
         if(user.username === username)
         {
-            console.log(`User ${username} already connected.`);
+            console.log(`User '${username} already connected.`);
             serveStatus(res, 401, 'Unauthorized access.');
             do_return = true;
         }
@@ -74,11 +74,10 @@ app.post('/login', (req, res) => {
     auth.authenticateUser(username, password).then(success => {
         if(success)
         {
-            console.log(`User ${username} authenticated.`);
+            console.log(`User '${username} authenticated.`);
             // Create short-lived token by default, make it long-lived if remember set to true
             const token_ttl = (remember === 'on') ? AUTH_TOKEN_TTL_S : AUTH_TOKEN_TTL_SHORT_LIVED_S;
             const token = auth.createAuthenticationToken(username, token_ttl);
-            addUser(username);
 
             res.cookie('username', username);
             res.cookie('auth_token', token);
@@ -86,7 +85,7 @@ app.post('/login', (req, res) => {
         }
         else
         {
-            console.log(`User ${username} failed to authenticate.`);
+            console.log(`User '${username}' failed to authenticate.`);
             serveStatus(res, 401, 'Authentication failed.');
         }
     });
@@ -126,26 +125,40 @@ app.get('/chat', (req, res) => {
     const decoded = auth.verifyAuthenticationToken(token);
     if(decoded && decoded.logged_in_as === username)
     {
-        console.log(`User ${username} authenticated by token.`);
-        addUser(username);
+        console.log(`User '${username}' authenticated by token.`);
         serveChatPage(res);
     }
     else
     {
-        console.log(`User ${username} failed to authenticate.`);
+        console.log(`User '${username}' failed to authenticate.`);
         serveLoginPage(res);
     }
 });
 
-io.on('connection', (socket) => {
-    console.log(`Handling user connection, socket ID: ${socket.id}`);
+// Handle handshake using a middleware
+io.use(function(socket, next) {
+    try
+    {
+        const handshakeData = JSON.parse(socket.handshake.query.handshakeData);
+        if(handshake(handshakeData, socket.id))
+        {
+            console.log(`Handshake complete for user '${handshakeData.username}'.`);
+            next();
+            return;
+        }
+    }
+    catch(err)
+    {
+        console.error(err.message);
+    }
+    next(new Error('Authentication error, handshake failed.'));
+});
 
+io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         removeUserBySocketID(socket.id);
         Sockets.delete(socket.id);
     });
-
-    socket.on(MSG_USER_ASSOCIATE, (pkt) => { associateUser(pkt.username, socket.id); });
 
     socket.on(MSG_CHAT_MESSAGE, (pkt) => {
         // Set date, base64 encode, save to DB and broadcast
@@ -165,16 +178,38 @@ io.on('connection', (socket) => {
                 names.set(msg.userid, auth.getUserName(msg.userid));
         });
         // Send base64 decoded messages back to client
-        const out = {history: []};
-        posts.forEach(function(msg) {
-            out.history.push({username : names.get(msg.userid), payload : base64decode(msg.body)});
-        });
+        const out = {history : []};
+        posts.forEach(function(
+            msg) { out.history.push({username : names.get(msg.userid), payload : base64decode(msg.body)}); });
 
         io.to(socket.id).emit(MSG_CHAT_HISTORY, out);
     });
 });
 
 server.listen(PORT, () => { console.log(`https://localhost:${PORT}`); });
+
+function handshake(handshakeData, socketid)
+{
+    console.log(`Beginning handshake for user '${username}'.`);
+
+    const username = handshakeData.username;
+    const token = handshakeData.token;
+    if(token && username)
+    {
+        const decoded = auth.verifyAuthenticationToken(token);
+        if(decoded && decoded.logged_in_as === username)
+        {
+            addUser(username);
+            Users.get(username).socketid = socketid;
+            Sockets.set(socketid, username);
+            console.log(`Associated user '${username}' to socket ID '${socketid}'`);
+            return true;
+        }
+    }
+
+    console.error(`Failed to associate user '${username}' with token '${token}'.`);
+    return false;
+}
 
 function addUser(username)
 {
@@ -195,7 +230,7 @@ function removeUserBySocketID(socketid)
         console.log(`User ${username} disconnected.`);
     }
     else
-        console.error(`Tried to disconnect unknown socket ID: ${socketid}`);
+        console.error(`Tried to unlist unknown socket ID: ${socketid}`);
 }
 
 function removeUserByName(username)
@@ -206,17 +241,7 @@ function removeUserByName(username)
         console.log(`User ${username} disconnected.`);
     }
     else
-        console.error(`Tried to disconnect unknown user: ${username}`);
-}
-
-function associateUser(username, socketid)
-{
-    // On client-side socket reconnect due to server restart, we need
-    // to add the user back
-    addUser(username);
-    Users.get(username).socketid = socketid;
-    Sockets.set(socketid, username);
-    console.log(`Associated user '${username}' to socket ID '${socketid}'`);
+        console.error(`Tried to unlist unknown user: ${username}`);
 }
 
 function serveStatus(res, sta, message) { res.status(sta).end(`<h1>${sta}</h1> <h2>${message}</h2>`); }
@@ -225,12 +250,6 @@ function serveLoginPage(res) { res.sendFile(path.join(__dirname, 'public/login.h
 
 function serveChatPage(res) { res.sendFile(path.join(__dirname, 'public/chat.html')); }
 
-function base64encode(str)
-{
-    return Buffer.from(str, 'utf8').toString('base64');
-}
+function base64encode(str) { return Buffer.from(str, 'utf8').toString('base64'); }
 
-function base64decode(str)
-{
-    return Buffer.from(str, 'base64').toString('utf8');
-}
+function base64decode(str) { return Buffer.from(str, 'base64').toString('utf8'); }
